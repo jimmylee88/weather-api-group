5d3e98efdfe5690a8b387f8fb24a7067090a45ae
Your Name <you@example.com>
2025-07-01 20:25:13 +0100

    resolve confilict

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,4 @@
-/*const API_KEY = "f23ee9deb4e1a7450f3157c44ed020e1";
 
-// First, get the latitude and longitude for the city
-const geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`;
-
-// Call getWeather API when the button is clicked
-const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;*/
-
-
-//document.getElementById("fetchRepos").addEventListener("click", onfetchRepos);
 document.getElementById("getWeather").addEventListener("click", async function() {
     const city = document.getElementById("city").value;
       console.log(city); // featch the city name from the input field
